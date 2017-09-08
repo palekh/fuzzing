1a786f83cea85e59413bc3c1d8e6534842b108ed
Igor <igolopolosov@gmail.com>
2017-09-09 01:41:53 +0300

    [up] 0.6.2 - add tests for fuzzer creation

--- src/fuzzerFactory/FunctionFuzzerFactory.spec.ts
+++ src/fuzzerFactory/FunctionFuzzerFactory.spec.ts
@@ -1,14 +1,26 @@
+import {FunctionFuzzer} from "../fuzzer/functionFuzzer";
 import {sum} from "./__mocks__/sum";
 import {functionFuzzer, FunctionFuzzerFactory} from "./FunctionFuzzerFactory";
 
 describe("FunctionFuzzerFactory", () => {
-    const sumFuzzerFactory: any = functionFuzzer(sum);
+    const sumFuzzerFactory = functionFuzzer(sum);
 
     test("functionFuzzer should create function fuzzer instance", () => {
         expect(sumFuzzerFactory instanceof FunctionFuzzerFactory).toBe(true);
     });
 
     test("functionFuzzer should create fuzzer with internal func", () => {
-        expect(sumFuzzerFactory.func).toBe(sum);
+        expect((sumFuzzerFactory as any).func).toBe(sum);
+    });
+
+    test("factory methods should creates fuzzer without error", () => {
+        const methodNames: Array<keyof FunctionFuzzerFactory> = [
+            "boolean", "booleanArray", "number", "numberArray", "string", "stringArray", "all",
+        ];
+
+        methodNames.forEach((method) => {
+            expect((sumFuzzerFactory as any)[method]() instanceof FunctionFuzzer).toBe(true);
+            expect(() => (sumFuzzerFactory as any)[method]()).not.toThrow();
+        });
     });
 });
