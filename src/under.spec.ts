e17e1a075c524204756c3829cbb2c83e301fa31d
Igor Golopolosov <igolopolosov@gmail.com>
2019-07-14 14:25:05 +0100

    ig -  lock unmber of errors in test

--- src/under.spec.ts
+++ src/under.spec.ts
@@ -5,17 +5,33 @@ import {fuzz, preset} from "./index";
 describe("under", () => {
     test("should accept presets", () => {
         const results = fuzz(sum)
-            .under(preset.string())
-            .all();
+            .under(preset.string());
 
-        expect(results).toMatchSnapshot();
+        const all = results.all();
+        const errors = results.errors();
+        const warnings = results.warnings();
+        const successes = results.successes();
+
+        expect(all).toMatchSnapshot();
+        expect(all).toHaveLength(11);
+        expect(errors).toHaveLength(2);
+        expect(warnings).toHaveLength(0);
+        expect(successes).toHaveLength(9);
     });
 
     test("should accept multiple presets", () => {
         const results = fuzz(multiply)
-            .under(preset.number(), preset.number())
-            .all();
+            .under(preset.number(), preset.number());
 
-        expect(results).toMatchSnapshot();
+        const all = results.all();
+        const errors = results.errors();
+        const warnings = results.warnings();
+        const successes = results.successes();
+
+        expect(all).toMatchSnapshot();
+        expect(all).toHaveLength(324);
+        expect(errors).toHaveLength(0);
+        expect(warnings).toHaveLength(76);
+        expect(successes).toHaveLength(248);
     });
 });
