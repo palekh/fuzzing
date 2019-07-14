eb7468e0b19f44f24e2ead12ea070de50331baf9
Igor Golopolosov <igolopolosov@gmail.com>
2019-07-14 13:57:32 +0100

    ig - add all combinations function

---
+++ src/fuzzer/__tests__/allCombinations.spec.ts
@@ -0,0 +1,50 @@
+import {allCombinations} from "../allCombinations";
+
+describe("allCombinations", () => {
+    test("should return all possible combinations for provided input", () => {
+        const input = [
+            ["c", true, {}],
+            [4, []]
+        ];
+        const output = [
+            ["c", 4],
+            [true, 4],
+            [{}, 4],
+            ["c", []],
+            [true, []],
+            [{}, []],
+        ];
+
+        expect(allCombinations(input)).toStrictEqual(output);
+    });
+
+    test("should return all possible combinations for complex input", () => {
+        const input = [
+            ["c", true, {}],
+            [4, []],
+            [NaN, null, Math.PI]
+        ];
+        const output = [
+            ["c", 4, NaN],
+            [true, 4, NaN],
+            [{}, 4, NaN],
+            ["c", [], NaN],
+            [true, [], NaN],
+            [{}, [], NaN],
+            ["c", 4, null],
+            [true, 4, null],
+            [{}, 4, null],
+            ["c", [], null],
+            [true, [], null],
+            [{}, [], null],
+            ["c", 4, Math.PI],
+            [true, 4, Math.PI],
+            [{}, 4, Math.PI],
+            ["c", [], Math.PI],
+            [true, [], Math.PI],
+            [{}, [], Math.PI],
+        ];
+
+        expect(allCombinations(input)).toStrictEqual(output);
+    });
+});
---
+++ src/fuzzer/allCombinations.ts
@@ -0,0 +1,25 @@
+
+export function allCombinations(input: any[][]): any[][] {
+    const counters = input.map(() => 0);
+
+    const output: any[][] = [];
+
+    while (counters.length === input.length) {
+        const iteration: any = [];
+        counters.forEach((counter, index) => {
+            iteration.push(input[index][counter]); 
+        });
+        output.push(iteration);
+    
+        counters[0] +=1;
+        counters.forEach((counter, index) => {
+            const max = input[index].length;
+            if (counter >= max) {
+                counters[index] = 0;
+                counters[index + 1] += 1;
+            }
+        });
+    }
+
+    return output;
+}
