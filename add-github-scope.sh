7cf39f4ed0ee3bf392b5a4f3d4a4e0b1eba03fbd
Igor Golopolosov <igolopolosov@gmail.com>
2020-07-03 21:00:27 +0100

    :construction_worker: Fix package name

--- add-github-scope.sh
+++ add-github-scope.sh
@@ -2,7 +2,7 @@
 
 scope() {
     echo '🚀 @palekh'
-    contents="$(jq '.name = "@palekh" + .name' ./package.json)" && \
+    contents="$(jq '.name = "@palekh/" + .name' ./package.json)" && \
     echo "${contents}" > ./package.json
 }
 
